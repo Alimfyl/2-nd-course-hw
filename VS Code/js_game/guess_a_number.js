ef72f2395881158fc874c21e2911a57ddb8769a8
Almir Ishmukhametov <alimfyl@gmail.com>
2025-11-17 23:06:14 +0500

    game mod1

--- VS Code/js_game/guess_a_number.js
+++ VS Code/js_game/guess_a_number.js
@@ -1,24 +1,41 @@
-function game () {
-function random ( min , max ) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    } 
-    let num = random ( 1 , 100 )
-    alert ("Угадайте число")
-    let numUser = prompt ("Введите своё число.")
-    while( numUser != num ) {
-        if(numUser < num) {
-        alert ("Число больше.")
-       numUser = prompt ("Введите своё число.")
-    }
-        else if  (numUser > num) ;{
-        alert ("Число меньше.")
-        numUser = prompt ("Введите своё число.")
-    }  
-         (numUser === num) ;{
-        alert ("Вы угадали!")
-        break;
+
+function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//function checkSpase() {
+//    
+//    if (numUser === null) {
+//        alert("Отменено!");
+//    }
+//    else {
+//        
+//    }
+//}
+
+function game() {
+    
+    let num = random(1, 100)
+    console.log(num)
+    alert("Угадайте число")
+    let numUser = prompt("Введите своё число.")
+    //checkSpase()
+    while (numUser != num) {
+        if (numUser < num) {
+            alert("Число больше.")
+             numUser = prompt("Введите своё число.")
+            //checkSpase()
+        }
+        else if (numUser > num) {
+            alert("Число меньше.")
+            numUser = prompt("Введите своё число.")
+            //checkSpase()
+        }
+        else if (numUser == null) {
+        alert("Отменено!");
+        break
         }
     }
-    }
+    alert("Вы угадали!")
+}
