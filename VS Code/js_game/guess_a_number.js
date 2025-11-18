6f3a0da10f162becffe6481321c1bc94af13a4eb
Almir Ishmukhametov <alimfyl@gmail.com>
2025-11-18 21:34:56 +0500

    Game +g

--- VS Code/js_game/guess_a_number.js
+++ VS Code/js_game/guess_a_number.js
@@ -4,38 +4,36 @@ function random(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-//function checkSpase() {
-//    
-//    if (numUser === null) {
-//        alert("Отменено!");
-//    }
-//    else {
-//        
-//    }
-//}
+
 
 function game() {
     
     let num = random(1, 100)
-    console.log(num)
     alert("Угадайте число")
-    let numUser = prompt("Введите своё число.")
-    //checkSpase()
-    while (numUser != num) {
-        if (numUser < num) {
-            alert("Число больше.")
-             numUser = prompt("Введите своё число.")
-            //checkSpase()
+
+    let input;
+    let attempts = 0;
+    
+
+    do {
+        let inputString = prompt('Введите своё число');
+        
+        
+        if (inputString === null) {
+            alert('Игра отменена!');
+            return;
         }
-        else if (numUser > num) {
-            alert("Число меньше.")
-            numUser = prompt("Введите своё число.")
-            //checkSpase()
+
+        input = +inputString;
+        attempts++;
+
+        if (isNaN(input)) {
+            alert('Это не число. Попробуйте еще раз.');
+        } else if (input !== num) {
+            alert(num > input ? 'Число больше.' : 'Число меньше.');
         }
-        else if (numUser == null) {
-        alert("Отменено!");
-        break
-        }
-    }
-    alert("Вы угадали!")
+        
+    } while (input !== num || isNaN(input));
+
+    alert(`Вы угадали!`);
 }
