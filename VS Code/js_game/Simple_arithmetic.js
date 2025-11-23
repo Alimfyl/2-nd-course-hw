7d4a04c383d709f2b415f4649fe91a27aedbde53
Almir Ishmukhametov <alimfyl@gmail.com>
2025-11-23 13:09:36 +0500

    simpl arifmetic+

--- VS Code/js_game/Simple_arithmetic.js
+++ VS Code/js_game/Simple_arithmetic.js
@@ -1,59 +1,62 @@
-
-        function rn(min, max) {
+function rn(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-    function gameArithmetic (){
-    function generate_example(lvl) {
 
-  const operator = {
-    0: ['+'],
-    1: ['-'],
-    2: ['*'],
-    3: ['/']
-  };
+function generateExample(lvl) {
+    const operatorsByLevel = {
+        0: ['+'],
+        1: ['-'],
+        2: ['*'],
+        3: ['/']
+    };
 
-    const availableOps = operator[lvl] || operator[1]; 
+    const availableOps = operatorsByLevel[lvl] || operatorsByLevel[0]; 
     const selectedOperator = availableOps[Math.floor(Math.random() * availableOps.length)];
 
-    let num1 = rn(1,20);
-    let num2 = rn(1,20);
+    let num1 = rn(1, 20);
+    let num2 = rn(1, 20);
 
     if (selectedOperator === '/') {
-        while(num2 === 0){
-            num2 = rn(1,20);
-        }
-    }
-    if (selectedOperator === '/'){
-        while(num1 % num2 != 0){
-            num1 = rn(1,20);
+        while (num2 === 0 || num1 % num2 !== 0) {
+            num1 = rn(1, 20);
+            num2 = rn(1, 20);
         }
     }
 
-  return `${num1} ${selectedOperator} ${num2}`;
+    const expression = `${num1} ${selectedOperator} ${num2}`;
+  
+    const answer = eval(expression); 
+
+    return { expression, answer };
 }
 
-let rnNum = rn(0,3)
-let result1 = generate_example(rnNum)
-let result2 = eval(result1)
-answer()
-console.log(result2);
 
-console.log(result1);
+function gameArithmetic() {
+    
+    const difficultyLevel = rn(0, 3);
+    const example = generateExample(difficultyLevel);
+    
+    
+    let userAnswer;
+    do {
+        
+        userAnswer = prompt(`Решите пример: ${example.expression}`);
 
-function answer(){
-alert(`Решите пример ${result1}`)
-userAnswer = prompt('Введите ответ');
-while(userAnswer != result2) {
-if (result2 != userAnswer){
-    alert('Неверно!')
-    userAnswer = prompt('Введите ответ');
-}
-else{
-    alert('Верно!')
-}
-}
-alert('Верно!')
-}
+        
+        if (userAnswer === null) {
+            alert('Игра отменена.');
+            return; 
+        }
+
+        
+        if (+userAnswer !== example.answer) {
+            alert('Неверно! Попробуйте еще раз.');
+        }
+
+    } while (+userAnswer !== example.answer); 
+    
+    alert('Верно!');
+    
 }
